5ed8f94e4a8f9868bee9d5430c7531e39de87715
haxorsprogramming <=>
2019-07-27 04:57:48 +0700

    Update migrate

---
+++ database/migrations/2019_07_26_215326_create_suppliers.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('kode');
+            $table->string('nama_lengkap');
+            $table->string('alamat');
+            $table->string('no_hp');
+            $table->string('email');
+            $table->string('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
