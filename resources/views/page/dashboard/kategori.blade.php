d94d1d5dc6a7fbbdba6900343b7b450fd4555e8c
haxorsprogramming <git@haxorsprogrammingclub.id>
2019-07-30 13:07:37 +0700

    data

--- resources/views/page/dashboard/kategori.blade.php
+++ resources/views/page/dashboard/kategori.blade.php
@@ -52,7 +52,7 @@ $(document).ready(function(){
     $('#divFormTambah').hide();
     $('.btnEdit').click(function(){
         var id = $(this).attr('id');
-        window.alert(id);
+        
     });
 
     $('#btnTampilForm').click(function(){
