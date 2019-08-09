a18f9618b1a52a7f62b39dfed7a1139d3a082fbd
haxorsprogramming <git@haxorsprogrammingclub.id>
2019-08-10 03:28:18 +0700

    seed
    Co-Authored-By: Aditia Darma Nasution <adityadarmanst@users.noreply.github.com>
    Co-Authored-By: Annastasya Purnama Nikita <annanikita@users.noreply.github.com>

--- database/migrations/2019_08_09_193502_buat_transaksi.php
+++ database/migrations/2019_08_09_193502_buat_transaksi.php
@@ -13,7 +13,17 @@ class BuatTransaksi extends Migration
      */
     public function up()
     {
-        //
+      Schema::create('tbl_transaksi', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->string('id_transaksi', 50);
+          $table->string('no_faktur', 50);
+          $table->string('jenis_transaksi', 20);
+          $table->string('kode_produk', 50);
+          $table->string('jumlah_barang', 7);
+          $table->string('total_biaya', 50);
+          $table->string('active', 1);
+          $table->timestamps();
+      });
     }
 
     /**
